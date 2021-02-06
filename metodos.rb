102a72e05cefde21298c4c077d377933fb6470c1
Felipe <lipeigansi@gmail.com>
2021-02-06 13:57:39 -0300

    Adicao dos arquivos hello, metodos e tipos

---
+++ metodos.rb
@@ -0,0 +1,39 @@
+# def imprime_nome(nome)
+#     puts nome
+# end
+
+# imprime_nome("Felipe Maciel")
+
+
+# def soma (valor1, valor2)
+#     puts valor1+valor2
+# end
+# soma(5,2)
+
+
+# def subtração (valor1, valor2)
+#     puts valor1 - valor2
+# end
+# subtração(5,2)
+
+
+# def multiplicacao (valor1, valor2)
+#     puts valor1 * valor2
+# end
+# multiplicacao(5,2)
+
+
+# def divisao (valor1, valor2)
+#     puts valor1 / valor2
+# end
+# divisao(5,2)
+
+
+def pega_soma(v1, v2)
+    total = v1+v2
+    return total
+end
+
+valor = pega_soma(3,4)
+
+puts valor
---
+++ tipos.rb
@@ -0,0 +1,6 @@
+nome = "Felipe"
+idade = 37
+legal = true
+
+puts "Nome: ",nome," Idade: ",idade," Status: ",legal
+puts "Nome: ",nome.class," Idade: ",idade.class," Status: ",legal.class
